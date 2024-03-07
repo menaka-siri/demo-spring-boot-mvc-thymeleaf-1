1f33c715b95b2d2fe45f4ffa1493d2f860725472
Menaka S <menaka442@gmail.com>
2024-03-07 07:26:24 +0530

    get checkbox item values from the props file

--- src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
@@ -18,6 +18,9 @@ public class StudentController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         Student theStudent = new Student();
@@ -27,6 +30,8 @@ public class StudentController {
         theModel.addAttribute("countries", countries);
 
         theModel.addAttribute("languages", languages);
+
+        theModel.addAttribute("systems", systems);
 
         return "student-form";
     }
