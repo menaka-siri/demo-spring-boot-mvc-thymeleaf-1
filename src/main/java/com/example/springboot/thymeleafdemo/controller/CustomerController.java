1892a5fb4e7f4f7804d0e3d91f23fbdc9b31bd20
Menaka S <menaka442@gmail.com>
2024-03-12 17:29:49 +0530

    add InitBinder preprocessor to trim whitespaces in string type fields

--- src/main/java/com/example/springboot/thymeleafdemo/controller/CustomerController.java
+++ src/main/java/com/example/springboot/thymeleafdemo/controller/CustomerController.java
@@ -2,15 +2,25 @@ package com.example.springboot.thymeleafdemo.controller;
 
 import com.example.springboot.thymeleafdemo.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/showCustomerForm")
     public String showCustomerForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
@@ -21,6 +31,8 @@ public class CustomerController {
     public String processCustomerForm(
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
+
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 
         if (theBindingResult.hasErrors()) {
             return "customer-form";
