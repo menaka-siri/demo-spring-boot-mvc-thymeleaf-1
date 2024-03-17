cb189f4cb5c1782368354e91a6a3b42802f315d0
Menaka S <menaka442@gmail.com>
2024-03-17 18:54:41 +0530

    make integer fields required in forms

--- src/main/java/com/example/springboot/thymeleafdemo/model/Customer.java
+++ src/main/java/com/example/springboot/thymeleafdemo/model/Customer.java
@@ -7,13 +7,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 3, message = "min length is 3")
     private String lastName;
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
-
-    public int getFreePasses() {
-        return freePasses;
-    }
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits allowed!")
     private String postalCode;
@@ -26,7 +23,11 @@ public class Customer {
         this.postalCode = postalCode;
     }
 
-    public void setFreePasses(int freePasses) {
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
