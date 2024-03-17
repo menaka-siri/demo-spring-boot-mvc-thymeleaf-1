5d2cf00bf7a2a283df59aef8e2fe926223445afe
Menaka S <menaka442@gmail.com>
2024-03-17 18:30:31 +0530

    add number range validation to freePasses field

--- src/main/java/com/example/springboot/thymeleafdemo/model/Customer.java
+++ src/main/java/com/example/springboot/thymeleafdemo/model/Customer.java
@@ -1,13 +1,22 @@
 package com.example.springboot.thymeleafdemo.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
     @NotNull(message = "is required")
     @Size(min=3, message = "min length is 3")
     private String lastName;
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value=10, message = "must be less than or equal to 10")
+    private int freePasses;
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
