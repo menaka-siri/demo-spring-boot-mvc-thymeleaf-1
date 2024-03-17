1195247ea3dcb6954e776112b86f35450dc56ee9
Menaka S <menaka442@gmail.com>
2024-03-17 18:37:01 +0530

    use regex for field validation

--- src/main/java/com/example/springboot/thymeleafdemo/model/Customer.java
+++ src/main/java/com/example/springboot/thymeleafdemo/model/Customer.java
@@ -5,13 +5,25 @@ import jakarta.validation.constraints.*;
 public class Customer {
     private String firstName;
     @NotNull(message = "is required")
-    @Size(min=3, message = "min length is 3")
+    @Size(min = 3, message = "min length is 3")
     private String lastName;
     @Min(value = 0, message = "must be greater than or equal to zero")
-    @Max(value=10, message = "must be less than or equal to 10")
+    @Max(value = 10, message = "must be less than or equal to 10")
     private int freePasses;
+
     public int getFreePasses() {
         return freePasses;
+    }
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits allowed!")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 
     public void setFreePasses(int freePasses) {
